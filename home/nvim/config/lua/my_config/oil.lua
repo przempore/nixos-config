8eeb13ddae6fdecb25680539b5213be4a54a959d
Przemek <przempore@gmail.com>
2023-12-08 23:16:33 +0100

    [home/nvim] update `oil` config

--- home/nvim/config/lua/my_config/oil.lua
+++ home/nvim/config/lua/my_config/oil.lua
@@ -6,9 +6,9 @@ require("oil").setup({
   -- See :help oil-columns
   columns = {
     "icon",
-    -- "permissions",
-    -- "size",
-    -- "mtime",
+    "permissions",
+    "size",
+    "mtime",
   },
   -- Buffer-local options to use for oil buffers
   buf_options = {
@@ -64,7 +64,7 @@ require("oil").setup({
   use_default_keymaps = true,
   view_options = {
     -- Show files and directories that start with "."
-    show_hidden = false,
+    show_hidden = true,
     -- This function defines what is considered a "hidden" file
     is_hidden_file = function(name, bufnr)
       return vim.startswith(name, ".")
@@ -135,4 +135,4 @@ require("oil").setup({
   },
 })
 
-vim.keymap.set("n", "<leader>pv", ":Oil --float<CR>", { noremap = true, silent = true, desc = "[misc] File explorer" })
+vim.keymap.set("n", "<leader>pv", ":Oil<CR>", { noremap = true, silent = true, desc = "[misc] File explorer" })
