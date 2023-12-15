a2cd61a39ec24f0c47c51ae9fa13cff4e60a5c39
Przemek <przempore@gmail.com>
2023-12-15 21:22:36 +0100

    [home/tmux] fix theme

--- home/tmux.nix
+++ home/tmux.nix
@@ -9,7 +9,26 @@
 
     plugins = with pkgs.tmuxPlugins; [
       better-mouse-mode
-      catppuccin
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_window_right_separator "█☠"
+          set -g @catppuccin_window_number_position "right"
+          set -g @catppuccin_window_middle_separator " | "
+
+          set -g @catppuccin_window_default_fill "none"
+          set -g @catppuccin_window_default_text "#W"
+
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_status_modules_right "application session user host date_time"
+          set -g @catppuccin_status_left_separator "█"
+          set -g @catppuccin_status_right_separator "█"
+
+          set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M:%S"
+        '';
+      }
       {
         plugin = resurrect;
         extraConfig = ''
@@ -61,22 +80,6 @@
 
       set -g status off
       bind b set-option -g status
-
-      set -g @catppuccin_window_right_separator "█☠"
-      set -g @catppuccin_window_number_position "right"
-      set -g @catppuccin_window_middle_separator " | "
-
-      set -g @catppuccin_window_default_fill "none"
-      set -g @catppuccin_window_default_text "#W"
-
-      set -g @catppuccin_window_current_fill "number"
-      set -g @catppuccin_window_current_text "#W"
-
-      set -g @catppuccin_status_modules_right "application session user host date_time"
-      set -g @catppuccin_status_left_separator "█"
-      set -g @catppuccin_status_right_separator "█"
-
-      set -g @catppuccin_date_time_text "%Y-%m-%d %H:%M:%S"
     '';
   };
 }
