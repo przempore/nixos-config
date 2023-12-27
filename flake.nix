c147f917265ef2a9d5da07019d301ffdb3750a45
Przemek <przempore@gmail.com>
2023-12-27 18:00:55 +0100

    [flake/home-manager] use nixpkgs 23.11

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
