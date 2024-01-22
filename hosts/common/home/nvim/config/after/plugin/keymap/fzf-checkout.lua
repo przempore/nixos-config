f62d5e35e10a40383c62ab8d759a2e0764f10690
Przemek <przempore@gmail.com>
2024-01-22 16:29:39 +0100

    [common/home/nvim] `<leader>gl` to open `git_commits` in telescope

--- hosts/common/home/nvim/config/after/plugin/keymap/fzf-checkout.lua
+++ hosts/common/home/nvim/config/after/plugin/keymap/fzf-checkout.lua
@@ -2,3 +2,7 @@ local nnoremap = require "my_config.keymap".nnoremap
 
 nnoremap("<leader>gc", ":GBranches<cr>", { noremap = true, silent = true, desc = "[Git] Branches"});
 nnoremap("<leader>gb", ":GBrowse<cr>", { noremap = true, silent = true, desc = "[Git] Browse"});
+
+local telescope = require('telescope.builtin')
+nnoremap("<leader>gl", function() telescope.git_commit() end,
+    { noremap = true, silent = true, desc = "[Git] show commits"});
