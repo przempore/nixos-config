8f206302f26628ade12dc0ba86930deb43f8dba4
Przemek <przempore@gmail.com>
2024-03-26 14:26:20 +0100

    [common/desktop] enable gtk setup

--- hosts/common/home/desktop/default.nix
+++ hosts/common/home/desktop/default.nix
@@ -6,4 +6,27 @@
     ./polybar
     ./screen_settings
   ];
+
+  gtk = {
+    enable = true;
+    iconTheme.package = pkgs.arc-icon-theme;
+    iconTheme.name = "Arc";
+    theme = {
+      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "pink" ];
+        size = "compact";
+        # tweaks = [ "rimless" "black" ];
+        tweaks = [ "rimless" ];
+        variant = "macchiato";
+      };
+    };
+  };
+
+  # Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+  xdg.configFile = {
+    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  };
 }
