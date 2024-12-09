490d02c0cf27682fc5ea5a39bec01e872f9dfc00
Przemek <przempore@gmail.com>
2024-12-09 18:44:23 +0100

    [Ilum] use default latest kernel

--- hosts/ilum/configuration.nix
+++ hosts/ilum/configuration.nix
@@ -14,7 +14,7 @@
 
   nix.settings.trusted-users = [ "root" "przemek" ];
 
-  boot.kernelPackages = pkgs.linuxPackages;
+  # boot.kernelPackages = pkgs.linuxPackages;
 
   networking.hostName = "ilum"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
