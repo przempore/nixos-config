4d2ac02aa9590e110cc9b3ba4b64e761ab6956f8
Przemek <przempore@gmail.com>
2025-01-08 21:30:49 +0100

    [openvpn] create a service

--- hosts/dooku/configuration.nix
+++ hosts/dooku/configuration.nix
@@ -22,20 +22,6 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
 
-  services.openvpn.servers = {
-    officeVPN = {
-      config = '' config /root/nixos/openvpn/officeVPN.conf '';
-      updateResolvConf = true;
-    };
-  };
-
-  # fileSystems."/mnt/office" = {
-  #   device = "//10.8.0.x/Share";
-  #   fsType = "cifs";
-  #   options = [ "noauto" "user" "uid=1000" "gid=100" "username=xxx" "password=xxx" "iocharset=utf8"
-  #     "x-systemd.requires=openvpn-officeVPN.service" ];
-  # };
-
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
@@ -53,6 +39,22 @@
     LC_TELEPHONE = "pl_PL.UTF-8";
     LC_TIME = "pl_PL.UTF-8";
   };
+
+  services.openvpn.servers = {
+    officeVPN = {
+      config = '' config /root/nixos/openvpn/officeVPN.conf '';
+      updateResolvConf = true;
+    };
+  };
+
+  # fileSystems."/mnt/office" = {
+  #   device = "//10.8.0.x/Share";
+  #   fsType = "cifs";
+  #   # options = [ "noauto" "user" "uid=1000" "gid=100" "username=xxx" "password=xxx" "iocharset=utf8"
+  #   #   "x-systemd.requires=openvpn-officeVPN.service" ];
+  #   options = [ "noauto" "user" "uid=1000" "gid=100" "credentials=/etc/samba/credentials" "iocharset=utf8"
+  #     "x-systemd.requires=openvpn-officeVPN.service" ];
+  # };
 
   services.qemuGuest.enable = true;
 
