05f94890139ca7edd6d685d63c11fcc179146398
Przemek <przempore@gmail.com>
2025-01-11 23:45:40 +0100

    make `pavucontrol` using _adwaita-dark_ theme

--- hosts/common/home/desktop/gtk.nix
+++ hosts/common/home/desktop/gtk.nix
@@ -1,5 +1,16 @@
 { pkgs, ... }:
 {
+  # dconf = {
+  #   settings = {
+  #     "org/gnome/desktop/interface" = {
+  #       # gtk-theme = "rose-pine";
+  #       # color-scheme = "prefer-dark";
+  #       gtk-theme = "rose-pine";
+  #       color-scheme = "prefer-dark";
+  #     };
+  #   };
+  # };
+
   gtk = {
     enable = true;
     theme = {
@@ -10,5 +21,23 @@
       name = "rose-pine";
       package = pkgs.rose-pine-icon-theme;
     };
+    # gtk3.extraConfig = {
+    #   gtk-application-prefer-dark-theme = true;
+    # };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
   };
+
+  # qt = {
+  #   enable = true;
+  #   style.name = "kvantum";
+  #   platformTheme.name = "kvantum";
+  # };
+  #
+  # xdg.portal = {
+  #   enable = true;
+  #   extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  #   configPackages = with pkgs; [ xdg-desktop-portal-gtk ];
+  # };
 }
