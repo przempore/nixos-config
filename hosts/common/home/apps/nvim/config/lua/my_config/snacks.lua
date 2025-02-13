995a7eb511cd16ae429d6e847377418bd84b13b5
Przemek <przempore@gmail.com>
2025-02-13 10:34:35 +0100

    [nvim/snacks] some changes

--- hosts/common/home/apps/nvim/config/lua/my_config/snacks.lua
+++ hosts/common/home/apps/nvim/config/lua/my_config/snacks.lua
@@ -7,21 +7,26 @@ local filtered_message = { "No information available" }
 require('snacks').setup({
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = { enabled = true },
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
     picker = { enabled = true },
-    notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
+    styles = {
+      notification = {
+        -- wo = { wrap = true } -- Wrap notifications
+      }
+    }
   },
 -- -- "folke/snacks.nvim",
 -- priority = 1000,
