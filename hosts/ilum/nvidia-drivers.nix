d6e773d43b3bb0eb826cb99d6d501dff29199aee
Przemek <przempore@gmail.com>
2025-03-17 13:17:52 +0100

    [ilum/nvidia] remove commented code

--- hosts/ilum/nvidia-drivers.nix
+++ hosts/ilum/nvidia-drivers.nix
@@ -31,12 +31,5 @@
       settingsSha256 = "sha256-LNL0J/sYHD8vagkV1w8tb52gMtzj/F0QmJTV1cMaso8=";
       usePersistenced = false;
     };
-    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #   version = "570.86.16"; # use new 570 drivers
-    #   sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
-    #   openSha256 = "sha256-DuVNA63+pJ8IB7Tw2gM4HbwlOh1bcDg2AN2mbEU9VPE=";
-    #   settingsSha256 = "sha256-9rtqh64TyhDF5fFAYiWl3oDHzKJqyOW3abpcf2iNRT8=";
-    #   usePersistenced = false;
-    # };
   };
 }
