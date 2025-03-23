732a635da6b75cfa4d9ae70916bc8cd682558095
Przemek <przempore@gmail.com>
2025-03-23 16:51:35 +0100

    [common/firefox] force search configuration | update `userChrome` revision

--- hosts/common/home/apps/firefox.nix
+++ hosts/common/home/apps/firefox.nix
@@ -14,7 +14,7 @@
             builtins.fetchGit {
                 url = "https://github.com/rockofox/firefox-minima";
                 ref = "main";
-                rev = "c5580fd04e9b198320f79d441c78a641517d7af5";
+                rev = "dc40a861b24b378982c265a7769e3228ffccd45a";
             }
           }/userChrome.css";
       '';
@@ -31,6 +31,7 @@
         "privacy.trackingprotection.socialtracking.enabled" = true;
       };
       search = {
+        force = true;
         default = "DuckDuckGo";
         engines = {
           "Nix Packages" = {
