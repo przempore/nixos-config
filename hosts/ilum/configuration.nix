6ca61f57328a465b599782b6f1885256044128af
Przemek <przempore@gmail.com>
2025-05-11 00:20:54 +0200

    [hyprland] add `wlsunset`

--- hosts/ilum/configuration.nix
+++ hosts/ilum/configuration.nix
@@ -51,6 +51,7 @@
     dunst # Notification daemon
     cliphist
     wl-clipboard
+    wlsunset
     xdg-desktop-portal-hyprland # For screen sharing, file dialogs
     qt5.qtwayland # Qt Wayland support
     qt6.qtwayland
