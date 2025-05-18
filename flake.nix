47b7ae5a70b93093e2da4905be52b038216fa5d0
Przemek <przempore@gmail.com>
2025-05-18 14:45:48 +0200

    [flake] remove commented out input

--- flake.nix
+++ flake.nix
@@ -20,7 +20,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    # zen-browser.url = "github:przempore/zen-browser-flake";
     tmux-sessionx = {
       url = "github:omerxx/tmux-sessionx";
       inputs.nixpkgs.follows = "nixpkgs";
