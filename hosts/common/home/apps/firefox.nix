dec1678d46add40d3c911bb2ac5e89d18344bfdd
Przemek <przempore@gmail.com>
2025-05-28 22:28:45 +0200

    [firefox] fix warning

--- hosts/common/home/apps/firefox.nix
+++ hosts/common/home/apps/firefox.nix
@@ -33,7 +33,7 @@
       };
       search = {
         force = true;
-        default = "DuckDuckGo";
+        default = "ddg";
         engines = {
           "Nix Packages" = {
             urls = [{
@@ -63,13 +63,13 @@
 
           "NixOS Wiki" = {
             urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
-            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            icon = "https://nixos.wiki/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@nw" ];
           };
 
-          "Bing".metaData.hidden = true;
-          "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+          "bing".metaData.hidden = true;
+          "google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
         };
       };
     };
