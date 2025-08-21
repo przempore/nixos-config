169073044bcad989f27fed02ed25f0271609fe12
Przemyslaw Porebski <porebski@adlares.com>
2025-08-21 13:18:24 +0200

    [common/fish] fix `asdf` function

--- hosts/common/home/apps/fish.nix
+++ hosts/common/home/apps/fish.nix
@@ -10,7 +10,7 @@
       ll = "eza --tree --level=1 --long --icons --git -lh";
       lah = "ll -lah";
       tree = "eza --tree";
-      asdf = lib.mkDefault "setxkbmap real-prog-dvorak";
+      asdf = lib.mkDefault "setxkbmap -option && setxkbmap -option ctrl:nocaps";
       asdfc = lib.mkDefault "setxkbmap -option ctrl:nocaps && setxkbmap -option altwin:swap_lalt_lwin";
       aoeu = lib.mkDefault "setxkbmap pl";
       sb = lib.mkDefault "cd ~/Projects/second-brain/; vi .";
