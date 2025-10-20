ec1797f776401f21d11976f400bb7f199612672b
Przemek <przempore@gmail.com>
2025-10-20 17:46:00 +0200

    [catppuccin] stub opitons to disable `delta`

--- hosts/common/home/catppuccin.nix
+++ hosts/common/home/catppuccin.nix
@@ -3,13 +3,27 @@
     "${home-manager-unstable}/modules/programs/vivid.nix"
     catppuccin.homeModules.catppuccin
   ];
-  catppuccin = {
-    enable = lib.mkDefault true;
-    flavor = "mocha";
-    accent = "pink";
-    cursors = {
-      enable = true;
-      accent = "dark";
+
+  options.programs.delta = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+    options = lib.mkOption {
+      type = lib.types.attrs;
+      default = {};
+    };
+  };
+
+  config = {
+    catppuccin = {
+      enable = lib.mkDefault true;
+      flavor = "mocha";
+      accent = "pink";
+      cursors = {
+        enable = true;
+        accent = "dark";
+      };
     };
   };
 }
