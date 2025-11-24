aae77145a5d751608b75982cd146b61541093f22
Przemek <przempore@gmail.com>
2025-11-24 21:39:52 +0100

    [zed] use fhs version

--- hosts/common/home/apps/zed/default.nix
+++ hosts/common/home/apps/zed/default.nix
@@ -3,8 +3,8 @@
 }: {
   home = {
     packages = [
-      pkgs-unstable.zed-editor
-      pkgs-unstable.zed-editor.remote_server
+      pkgs-unstable.zed-editor-fhs
+      # pkgs-unstable.zed-editor.remote_server
     ];
 
     file.".config/zed/settings.json".source = ./settings.json;
