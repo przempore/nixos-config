0b4d0ae0e8382dd684179051291a1b814f38e666
Przemek <przempore@gmail.com>
2025-11-27 22:49:17 +0100

    [lix] switch to `main` branch

--- flake.nix
+++ flake.nix
@@ -18,10 +18,21 @@
     home-manager-unstable.url = "github:nix-community/home-manager";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     # mozilla-overlay.url = "github:mozilla/nixpkgs-mozilla"; # not used anymore
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
+    lix = {
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
     };
+    
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
+    };
+
+    # lix-module = {
+    #   url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     tmux-sessionx = {
       url = "github:omerxx/tmux-sessionx";
