4045e7945946c12d55decd8784c5bef6b7affc80
Przemek <przempore@gmail.com>
2025-12-05 22:06:58 +0100

    [hyprland] toggle waybar

--- hosts/common/home/desktop/hyprland/config/scripts/toggle_waybar.sh
+++ hosts/common/home/desktop/hyprland/config/scripts/toggle_waybar.sh
@@ -2,27 +2,10 @@
 
 echo "[toggle_waybar] start" | systemd-cat
 
-state_file="$HOME/.cache/waybar_state"
-
-# Check if the state file exists
-if [ ! -f "$state_file" ]; then
-    echo "visible" > "$state_file"
-fi
-
-# Read the current state
-current_state=$(cat "$state_file")
-
-# Toggle the state
-if [[ "$current_state" == "visible" ]]; then
-    # Hide Waybar and adjust Hyprland gaps
-    killall waybar
-    hyprctl keyword general:gaps_out 0
-    echo "hidden" > "$state_file"
+if pgrep -x .waybar-wrapped >/dev/null; then
+  pkill -x .waybar-wrapped
 else
-    # Show Waybar and adjust Hyprland gaps
-    waybar &
-    hyprctl keyword general:gaps_out 4
-    echo "visible" > "$state_file"
+  waybar >/dev/null 2>&1 &
 fi
 
 echo "[toggle_waybar] end" | systemd-cat
