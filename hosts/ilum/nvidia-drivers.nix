c587feb4f75f49f4eadf5e4a795aa1dcf7ee3b02
Przemek <przempore@gmail.com>
2025-12-14 19:30:31 +0100

    [nvidia] use _beta_ drivers after kernel update

--- hosts/ilum/nvidia-drivers.nix
+++ hosts/ilum/nvidia-drivers.nix
@@ -26,17 +26,17 @@
     open = true;
     # package = config.boot.kernelPackages.nvidiaPackages.latest;
     # https://github.com/NixOS/nixpkgs/issues/467145#issuecomment-3603995380
-    package = config.boot.kernelPackages.nvidiaPackages.stable // {
-      open = config.boot.kernelPackages.nvidiaPackages.stable.open.overrideAttrs (old: {
-        patches = (old.patches or [ ]) ++ [
-          (pkgs.fetchpatch {
-            name = "get_dev_pagemap.patch";
-            url = "https://github.com/NVIDIA/open-gpu-kernel-modules/commit/3e230516034d29e84ca023fe95e284af5cd5a065.patch";
-            hash = "sha256-BhL4mtuY5W+eLofwhHVnZnVf0msDj7XBxskZi8e6/k8=";
-          })
-        ];
-      });
-    };
+    # package = config.boot.kernelPackages.nvidiaPackages.stable // {
+    #   open = config.boot.kernelPackages.nvidiaPackages.stable.open.overrideAttrs (old: {
+    #     patches = (old.patches or [ ]) ++ [
+    #       (pkgs.fetchpatch {
+    #         name = "get_dev_pagemap.patch";
+    #         url = "https://github.com/NVIDIA/open-gpu-kernel-modules/commit/3e230516034d29e84ca023fe95e284af5cd5a065.patch";
+    #         hash = "sha256-BhL4mtuY5W+eLofwhHVnZnVf0msDj7XBxskZi8e6/k8=";
+    #       })
+    #     ];
+    #   });
+    # };
     # https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/linux/nvidia-x11/default.nix
     # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     #   version = "575.64.05";
@@ -46,7 +46,7 @@
     #   persistencedSha256 = "sha256-2g5z7Pu8u2EiAh5givP5Q1Y4zk4Cbb06W37rf768NFU=";
     # };
     # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     #   version = "570.124.04";
     #   sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
