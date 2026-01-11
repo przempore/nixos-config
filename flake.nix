53c085595c20eb1f45ee44404b989c857a42a1c0
Przemek <przempore@gmail.com>
2026-01-12 00:45:47 +0100

    [flake] remove whitespaces

--- flake.nix
+++ flake.nix
@@ -22,7 +22,6 @@
       url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
       flake = false;
     };
-    
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
