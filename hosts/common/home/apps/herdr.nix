4fd3c715ec499b68d1dc0180972b070eadd1ff2c
Przemek <przempore@gmail.com>
2026-09-05 02:36:10 +0200

    [herdr] fix tab shortcuts after reordering

--- hosts/common/home/apps/herdr.nix
+++ hosts/common/home/apps/herdr.nix
@@ -38,52 +38,52 @@
     [[keys.command]]
     key = "prefix+0"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[0].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[0].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+1"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[1].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[1].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+2"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[2].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[2].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+3"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[3].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[3].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+4"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[4].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[4].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+5"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[5].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[5].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+6"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[6].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[6].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+7"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[7].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[7].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+8"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[8].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[8].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
 
     [[keys.command]]
     key = "prefix+9"
     type = "shell"
-    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs | sort_by(.number) | .[9].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
+    command = 'tab_id="$(herdr tab list --workspace "$HERDR_ACTIVE_WORKSPACE_ID" | jq -r ".result.tabs[9].tab_id // empty")" && [ -n "$tab_id" ] && herdr tab focus "$tab_id"'
   '';
 
 }
